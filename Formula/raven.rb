b8c623b300048287d76e05fc9a66b7e4510dea3d
Brianna Zamora <briannadoubt@icloud.com>
2026-02-07 20:05:39 -0800

    Update raven formula to 0.10.1 release tarball

--- Formula/raven.rb
+++ Formula/raven.rb
@@ -1,8 +1,9 @@
 class Raven < Formula
   desc "SwiftUI to DOM cross-compiler"
   homepage "https://github.com/briannadoubt/Raven"
-  url "https://github.com/briannadoubt/Raven.git", branch: "main"
-  version "0.10.0"
+  url "https://github.com/briannadoubt/Raven/archive/refs/tags/0.10.1.tar.gz"
+  sha256 "2a8e966265fe6ae4921ac50ec4fd031fb95fc4ea9c4349d2c90dc34750cd297f"
+  version "0.10.1"
   license "Apache-2.0"
   head "https://github.com/briannadoubt/Raven.git", branch: "main"
 
