5f2cebf32018e0809c90c20a41e0708cd17c14f5
Brianna Zamora <briannadoubt@icloud.com>
2026-02-09 21:17:31 -0800

    raven 0.10.3

--- Formula/raven.rb
+++ Formula/raven.rb
@@ -1,9 +1,9 @@
 class Raven < Formula
   desc "SwiftUI to DOM cross-compiler"
   homepage "https://github.com/briannadoubt/Raven"
-  url "https://github.com/briannadoubt/Raven/archive/refs/tags/0.10.2.tar.gz"
-  sha256 "1d25430cdea9c7e4c107e7ef6dacb1f887ed8218ea55149755ff0dc2e9ca9175"
-  version "0.10.2"
+  url "https://github.com/briannadoubt/Raven/archive/refs/tags/0.10.3.tar.gz"
+  sha256 "5df106266bbe028bd91204aa3e67f2d1747cb20148487c3b6dcafb2edc18054b"
+  version "0.10.3"
   license "Apache-2.0"
   head "https://github.com/briannadoubt/Raven.git", branch: "main"
 
